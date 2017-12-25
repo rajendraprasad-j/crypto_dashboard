84a27cc8adb75aa86643cf67825fa368df931b0a
rajendra prasad <rajendraprasad.jeevanantham@mindtree.com>
2017-12-26 00:00:21 +0530

    changes

--- src/Components/MyAccounts/MyAccounts.js
+++ src/Components/MyAccounts/MyAccounts.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { headShake } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
 
-
+import './myaccount.css';
+const styles = {
+  bounce: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(headShake, 'headShake')
+  }
+}
 class MyAccounts extends Component {
   render() {
     return (
-      <div className="balance_container">
+      <div className="accounts_container">
         <div className="sub_titles">MyAccounts<span></span></div>
+        <div className="card_container">
+        <StyleRoot>
+          <div className="acc_card_main" style={styles.bounce}>
+            <div className="acc_name">Bitflinex</div>
+            <div className="card_number">1BvB **** **** ****</div>
+            <div className ="card_add_details">
+              <div>BTC\UTC</div>
+              <div className="card_type">Brandon Lucas</div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+          </StyleRoot>
+          <div className="acc_card_secondary" style={styles.bounce}>
+            <div className="acc_name">Bitflinex</div>
+            <div className="card_number">1BvB **** **** ****</div>
+            <div className ="card_add_details">
+              <div>BTC\UTC</div>
+              <div className="card_type">Brandon Lucas</div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="acc_card_secondary" style={styles.bounce}>
+            <div className="acc_name">Bitflinex</div>
+            <div className="card_number">1BvB **** **** ****</div>
+            <div className ="card_add_details">
+              <div>BTC\UTC</div>
+              <div className="card_type">Brandon Lucas</div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>'
+        </div>
       </div>
     );
   }
